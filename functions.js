51146395ae5dc172288f122bf8f5e2bd4b25a6c8
Cesar David Revelo <ing.revelo@gmail.com>
2016-09-08 21:43:29 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,9 +2,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() { 
   $('#slideshow > div:first')
-    .fadeOut(3000)
+    .fadeOut(1000)
     .next()
-    .fadeIn(3000)
+    .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
-},  1000);
+},  3000);
